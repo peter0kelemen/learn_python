2514d68a303f17a2421fe5e492f900bb48fa1eec
Peter Kelemen <peter0kelemen@gmail.com>
2019-01-06 20:00:28 +0100

    Committing the first file versions

---
+++ learn.py
@@ -0,0 +1,13 @@
+'''
+csak kellene az a komment na
+'''
+
+kor = 45
+nem = "no"
+
+if nem != "ferfi":
+    print("lanykent nem kell aggodni")
+elif kor > 40 and nem == "ferfi":
+    print("orvosi vizsgalat kellene")
+else:
+    print("fiatal vagy meg ehhez")
